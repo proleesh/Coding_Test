476893884e6a8568ee887da14e930d3b095b30da
Sunghyuk Lee <57159010+proleesh@users.noreply.github.com>
2022-02-28 16:37:38 +0900

    Update ObjectClassMain.py

--- ObjectClassMain.py
+++ ObjectClassMain.py
@@ -7,7 +7,7 @@ def main():
     stu1.study("파이썬을 배우고 있습니다.") #study에게 알림을 보내준다
     stu1.watch_movie("전쟁영화를 시청하고 있습니다.") #watch_movie에게 알림을 보내준다
 
-    stu2 = Student("하늘", 29)
+    stu2 = Student("하하", 32)
     stu2.study("신기술을 배우고 있습니다. ")
     stu2.watch_movie("공포영화를 시청하고 있습니다.")
 
