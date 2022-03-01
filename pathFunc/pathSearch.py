64d3a3cfd58d8b532fe96d52ccc34ce7e0ffe8a1
Sunghyuk Lee <57159010+proleesh@users.noreply.github.com>
2022-03-01 16:37:55 +0900

    used module
    import function module.module_function(String)

---
+++ pathFunc/pathSearch.py
@@ -0,0 +1,5 @@
+#导入模块
+import support
+#现在可以调用模块里包含的函数
+#代码：模块.模块中的方法(想要输出的文字)
+support.print_func("Hello World")
---
+++ pathFunc/support.py
@@ -0,0 +1,4 @@
+#预先设定模块
+def print_func(par):
+    print("Hello : ", par)
+    return
