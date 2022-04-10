4d1d0d20cb3d638db982ea979e11638555c26e84
Sunghyuk Lee <57159010+proleesh@users.noreply.github.com>
2022-04-10 22:09:05 +0900

    add&fixed code

--- index007/main_7_t.js
+++ index007/main_7_t.js
@@ -3,15 +3,29 @@ const person = {
     lastName: "Lee",
     university: "Semyung University",
     student_id: 2022240029,
+    fullName: function(){
+        return this.firstName + " " + this.lastName;
+    }
 }
+console.log(person.fullName())//콘솔에서 Sunghyuk Lee 출력
 
+// alert(person.fullName());//Sunghyuk Lee 창이 뜬다
 //객체의 일부 데이터 표시
 //innerHTML 내부HTML
 document.getElementById("name").innerHTML = 
-person.firstName + " " + person.lastName;//브라우저에서 Sunghyuk Lee 출력
+// person.firstName + " " + person.lastName;//브라우저에서 Sunghyuk Lee 출력
+person.fullName(); //똑같이 Sunghyuk Lee 출력
 
 document.getElementById('university').innerHTML =
 person['university'];//브라우저에서 Semyung University 출력
 
 document.getElementById('stu_id').innerHTML = 
 person['student_id']; //브라우저에서 2022240029 출력
+
+function displayDate(){
+    document.getElementById('date_2').innerHTML = 
+    Date();
+}
+
+const text = '대한민국'
+document.getElement
