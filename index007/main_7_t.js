454cc08c92dd0af2f74223bde922138231161db4
Sunghyuk Lee <57159010+proleesh@users.noreply.github.com>
2022-04-10 22:57:44 +0900

    add code

--- index007/main_7_t.js
+++ index007/main_7_t.js
@@ -23,9 +23,34 @@ document.getElementById('stu_id').innerHTML =
 person['student_id']; //브라우저에서 2022240029 출력
 
 function displayDate(){
-    document.getElementById('date_2').innerHTML = 
-    Date();
+    document.getElementById('date_2').innerHTML = Date();
 }
 
 const text = '대한민국'
-document.getElement
+// document.querySelector('#phrases').innerHTML = text; //대한민국
+const length = text.length;
+console.log(length) //4 문자의 길이
+
+document.querySelector('#phrases').innerHTML = typeof text; //string
+//typeof 자료형을 출력
+
+
+const log = document.querySelector('#mouseClick_log');
+
+document.addEventListener('mousemove', inputChange)
+
+function inputChange(e){
+    log.innerText = `Client Position: ${e.clientX}, ${e.clientY}
+                     Screen Position: ${e.screenX}, ${e.screenY}`
+}
+
+const x = new String('Kakao')
+const y = new String('Naver')
+const num_1 = new Int16Array(10)
+const num_2 = new Int16Array(100)
+
+ document.getElementById('typeof').innerHTML = 
+ (x === y) //false 즉 Kakao 와 Naver는 같지않다
+
+document.getElementById('typeof_1').innerHTML =
+(num_1 < num_2)//true 즉 num_1은 num_2보다 작다
