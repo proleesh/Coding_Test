12f5e5e96b0696d467270ae989c3139d906bdb62
Sunghyuk Lee <57159010+proleesh@users.noreply.github.com>
2022-04-29 18:16:23 +0900

    fixed error

--- Smart2022A/09/0429_3/clock.js
+++ Smart2022A/09/0429_3/clock.js
@@ -15,7 +15,7 @@ function getTime(){
     const hours  = date.getHours() //시간을 가져오다
     const minutes = date.getMinutes() //분을 가져오다
     const seconds = date.getSeconds() //초를 가져오다
-    clockTitle.innerText = // < 10 이유는 19이하숫자 앞에 0을 붙이고 싶어서 이렇게 쓴다 그리고 0$() 괄호가 아니다 0${} brace key 다. 물론 틀리게 써도 오류는 안 난다. 근데 $(object)로 표시된다
+    clockTitle.innerText = // < 10 이유는 10이하숫자 앞에 0을 붙이고 싶어서 이렇게 쓴다 그리고 0$() 괄호가 아니다 0${} brace key 다. 물론 틀리게 써도 오류는 안 난다. 근데 $(object)로 표시된다
     `${hours < 10 ? `0${hours}` : hours} : ${minutes < 10 ? `0${minutes}` : minutes} : ${seconds < 10 ? `0${seconds}` : seconds}`
     
 }
