55c00b2e873e5f6dbc24273997d987225a30b936
Sunghyuk Lee <57159010+proleesh@users.noreply.github.com>
2022-05-27 18:19:24 +0900

    added description

--- Smart2022A/13/0527/app4.js
+++ Smart2022A/13/0527/app4.js
@@ -4,7 +4,8 @@ const color = document.getElementById('color');
 
 document.body.addEventListener("click", ()=>{
     console.log("1, body")
-})
+})//body tag를 addEventListener로 출력했을 때 반드시 document.body로 알려줘야 한다. 컴퓨터는 모르니까!
+//예시: document.body.addEventListener()
 box.addEventListener("click",()=>{
     console.log("2, div")
 })
