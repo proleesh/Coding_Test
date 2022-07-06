0a160a4c8ece4bc31ffb5f297932b73918129e33
Sunghyuk Lee <57159010+proleesh@users.noreply.github.com>
2022-07-06 21:55:44 +0900

    Add files via upload

---
+++ SpringProject/Naengmyeon/src/main/java/com/example/naengmyeon/init/DestructiveBeanConfigDemo.java
@@ -0,0 +1,30 @@
+package com.example.naengmyeon.init;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.support.GenericApplicationContext;
+
+public class DestructiveBeanConfigDemo {
+	@Configuration
+	static class DestructiveBeanConfig{
+		@Lazy
+		@Bean(initMethod = "afterPropertiesSet", destroyMethod = "destroy")
+		DestructiveBean destructiveBean() {
+			DestructiveBean destructiveBean = new DestructiveBean();
+			destructiveBean.setFilePath(System.getProperty("java.io.tmpdir") + 
+					System.getProperty("file.separator") + "test.txt");
+			return destructiveBean;	
+		}
+	}
+	public static void main(String[] args) {
+		GenericApplicationContext ctx = new AnnotationConfigApplicationContext(DestructiveBeanConfig.class);
+		ctx.getBean(DestructiveBean.class);
+		System.out.println("destroy() 호출 시작");
+		ctx.destroy();
+		System.out.println("destroy() 호출 종료");
+	}
+	
+
+}
